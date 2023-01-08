f36b25829b2f56756b5723d2130bfdb2095ca7ea
Mohd <mohd.ali.el@gmail.com>
2023-01-08 16:30:18 +0000

    added build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "building jarmonik";
+source ~/.bashrc
+cd /root/jarmonik.org
+rm -rf public
+git pull
+go run makesite.go
+rm -rf /var/www/jarmonik.org/public
+cp -R ./public /var/www/jarmonik.org/public
